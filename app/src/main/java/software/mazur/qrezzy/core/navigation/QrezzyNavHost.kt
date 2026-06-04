9fb79184b36a04405956b22b527bb849f88f208d
Slawek Mazur <161143611+slawekmazur@users.noreply.github.com>
2026-06-04 20:14:38 +0200

    feat(navigation): add navigation host

---
+++ app/src/main/java/software/mazur/qrezzy/core/navigation/QrezzyNavHost.kt
@@ -0,0 +1,48 @@
+package software.mazur.qrezzy.core.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import software.mazur.qrezzy.feature.generator.GeneratorScreen
+import software.mazur.qrezzy.feature.history.HistoryScreen
+import software.mazur.qrezzy.feature.onboarding.OnboardingScreen
+import software.mazur.qrezzy.feature.scanner.ScannerScreen
+import software.mazur.qrezzy.feature.settings.SettingsScreen
+import software.mazur.qrezzy.feature.splash.SplashScreen
+
+@Composable
+fun QrezzyNavHost() {
+
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = QrezzyDestination.Scanner.route,
+    ) {
+
+        composable(QrezzyDestination.Splash.route) {
+            SplashScreen()
+        }
+
+        composable(QrezzyDestination.Onboarding.route) {
+            OnboardingScreen()
+        }
+
+        composable(QrezzyDestination.Scanner.route) {
+            ScannerScreen()
+        }
+
+        composable(QrezzyDestination.Generator.route) {
+            GeneratorScreen()
+        }
+
+        composable(QrezzyDestination.History.route) {
+            HistoryScreen()
+        }
+
+        composable(QrezzyDestination.Settings.route) {
+            SettingsScreen()
+        }
+    }
+}
