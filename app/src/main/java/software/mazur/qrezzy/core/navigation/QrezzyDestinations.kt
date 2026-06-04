923f580ea00bb91bcc0ff3186a792fbf7c982268
Slawek Mazur <161143611+slawekmazur@users.noreply.github.com>
2026-06-04 20:05:56 +0200

    feat(navigation): add application destinations

---
+++ app/src/main/java/software/mazur/qrezzy/core/navigation/QrezzyDestinations.kt
@@ -0,0 +1,12 @@
+package software.mazur.qrezzy.core.navigation
+
+sealed class QrezzyDestination(
+    val route: String
+) {
+    data object Splash : QrezzyDestination("splash")
+    data object Onboarding : QrezzyDestination("onboarding")
+    data object Scanner : QrezzyDestination("scanner")
+    data object Generator : QrezzyDestination("generator")
+    data object History : QrezzyDestination("history")
+    data object Settings : QrezzyDestination("settings")
+}
