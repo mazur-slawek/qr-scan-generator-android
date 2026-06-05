62b8724ee3245c40a1476e346f15ccb076162117
Slawek Mazur <161143611+slawekmazur@users.noreply.github.com>
2026-06-05 13:11:03 +0200

    feat(splash): add animated splash screen

--- app/src/main/java/software/mazur/qrezzy/feature/splash/SplashScreen.kt
+++ app/src/main/java/software/mazur/qrezzy/feature/splash/SplashScreen.kt
@@ -1,9 +1,23 @@
 package software.mazur.qrezzy.feature.splash
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import software.mazur.qrezzy.core.designsystem.components.QrezzyBranding
+import software.mazur.qrezzy.core.designsystem.components.QrezzyAnimatedBackground
 
 @Composable
 fun SplashScreen() {
-    QrezzyBranding()
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        QrezzyAnimatedBackground()
+        QrezzyBranding(modifier = Modifier.padding(horizontal = 16.dp)
+        )
+    }
 }
