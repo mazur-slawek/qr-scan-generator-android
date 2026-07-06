99307c21141c3086108ff31ef9c6ef2f0e315b6a
Slawek Mazur <161143611+slawekmazur@users.noreply.github.com>
2026-07-06 18:49:44 +0200

    chore(crashlytics): add diagnostic logs

--- app/src/main/java/software/mazur/qrezzy/QrezzyApplication.kt
+++ app/src/main/java/software/mazur/qrezzy/QrezzyApplication.kt
@@ -2,6 +2,17 @@ package software.mazur.qrezzy
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import software.mazur.qrezzy.core.common.crash.CrashReporter
+import javax.inject.Inject
 
 @HiltAndroidApp
-class QrezzyApplication : Application()
+class QrezzyApplication : Application() {
+    @Inject
+    lateinit var crashReporter: CrashReporter
+
+    override fun onCreate() {
+        super.onCreate()
+        crashReporter.setEnabled(!BuildConfig.DEBUG)
+        crashReporter.log("QREZZY application started")
+    }
+}
