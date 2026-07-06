e9812c2c9fda08d9119f60edbecf973afca9cf18
Slawek Mazur <161143611+slawekmazur@users.noreply.github.com>
2026-07-06 16:57:38 +0200

    test: cover edge cases in QrInputMapper

--- app/src/test/java/software/mazur/qrezzy/feature/generator/mapper/QrInputMapperTest.kt
+++ app/src/test/java/software/mazur/qrezzy/feature/generator/mapper/QrInputMapperTest.kt
@@ -123,4 +123,92 @@ class QrInputMapperTest {
         val result = input.toQrContent()
         assertEquals("", result)
     }
+
+    @Test
+    fun `should map wifi input with WEP encryption`() {
+        val input = QrInput.Wifi(ssid = " Home ", password = " 12345 ", encryption = WifiEncryption.WEP, hidden = false)
+        val result = input.toQrContent()
+        assertEquals("WIFI:T:WEP;S:Home;P:12345;H:false;;", result)
+    }
+
+    @Test
+    fun `should map wifi input with NONE encryption without password`() {
+        val input = QrInput.Wifi(ssid = " Net work ", password = " ignored ", encryption = WifiEncryption.NONE, hidden = false)
+        val result = input.toQrContent()
+        assertEquals("WIFI:T:nopass;S:Net work;H:false;;", result)
+    }
+
+    @Test
+    fun `should escape special characters in wifi content`() {
+        val input =
+            QrInput.Wifi(ssid = " Home;WiFi:Main ", password = """ pa\ss;word:123 """, encryption = WifiEncryption.WPA, hidden = true)
+        val result = input.toQrContent()
+        assertEquals("""WIFI:T:WPA;S:Home\;WiFi\:Main;P:pa\\ss\;word\:123;H:true;;""", result)
+    }
+
+    @Test
+    fun `should map contact input with first name and last name`() {
+        val input = QrInput.Contact(firstName = " Sławek ", lastName = " Mazur ")
+        val result = input.toQrContent()
+        assertEquals(
+            """
+        BEGIN:VCARD
+        VERSION:3.0
+        N:Mazur;Sławek
+        FN:Sławek Mazur
+        END:VCARD
+        
+            """.trimIndent(),
+            result
+        )
+    }
+
+    @Test
+    fun `should map contact input with only phone`() {
+        val input = QrInput.Contact(phone = " +48123456789 ")
+        val result = input.toQrContent()
+        assertEquals(
+            """
+        BEGIN:VCARD
+        VERSION:3.0
+        N:;
+        FN:
+        TEL:+48123456789
+        END:VCARD
+
+            """.trimIndent(),
+            result
+        )
+    }
+
+    @Test
+    fun `should encode sms body with spaces and special characters`() {
+        val input = QrInput.Sms(
+            phoneNumber = " +48123456789 ",
+            message = " QREZZY TEST? a=1&b=2 "
+        )
+        val result = input.toQrContent()
+        assertEquals("sms:+48123456789?body=QREZZY+TEST%3F+a%3D1%26b%3D2", result)
+    }
+
+    @Test
+    fun `should map email input with empty subject and body`() {
+        val input = QrInput.Email(email = " contact@test.com ", subject = " ", body = " ")
+        val result = input.toQrContent()
+        assertEquals("MATMSG:TO:contact@test.com;SUB:;BODY:;;", result)
+    }
+
+    @Test
+    fun `should return empty content when geo latitude is blank`() {
+        val input = QrInput.GeoLocation(latitude = " ", longitude = "16.9252")
+        val result = input.toQrContent()
+        assertEquals("", result)
+    }
+
+    @Test
+    fun `should return empty content when geo longitude is blank`() {
+        val input = QrInput.GeoLocation(latitude = "52.4064", longitude = " ")
+        val result = input.toQrContent()
+        assertEquals("", result)
+    }
 }
