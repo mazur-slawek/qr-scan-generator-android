c33ca7590401ab546e29e597818db0459b609aaa
Slawek Mazur <161143611+slawekmazur@users.noreply.github.com>
2026-07-31 11:19:03 +0200

    build: add lifecycle-runtime-compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ android {
 
 dependencies {
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
