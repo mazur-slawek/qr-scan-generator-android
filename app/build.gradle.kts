27f6312bca6ebcfad9b0b63f265380aff894fac0
Slawek Mazur <161143611+slawekmazur@users.noreply.github.com>
2026-07-31 14:30:53 +0200

    chore: prepare 2.0.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "software.mazur.qrezzy"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "2.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
