0a9a39e4a73ca0b03668677c7ba3eefc96a77cf3
Slawek Mazur <161143611+slawekmazur@users.noreply.github.com>
2026-08-14 17:05:06 +0200

    fix: localize scanner and generator errors

--- app/src/main/java/software/mazur/qrezzy/feature/scanner/model/ScannerUiEvent.kt
+++ app/src/main/java/software/mazur/qrezzy/feature/scanner/model/ScannerUiEvent.kt
@@ -2,5 +2,6 @@ package software.mazur.qrezzy.feature.scanner.model
 
 sealed interface ScannerUiEvent {
     data object QrSaved : ScannerUiEvent
-    data class ShowError(val message: String) : ScannerUiEvent
+    data object QrSaveFailed : ScannerUiEvent
+    data object NoQrFoundInImage : ScannerUiEvent
 }
